0b1ba06a52cd81c86722c81f57d90415274f094e
Devin Michaels <d.michaels@miami.edu>
2017-03-17 19:36:07 -0400

    Transferred work from examples into final files and deleted examples

--- project2/RandomizeInPlace.js
+++ project2/RandomizeInPlace.js
@@ -1,55 +1,94 @@
-function setCellColor(cell, color) {
-  document.getElementById('a'+cell).style.background = color ;
-}
+var cb_color  = "#ffe" ; //code background
+var cbh_color = "#9f7" ; //code background highlight
+
+var step ;
+var index;
+var swapIndex;
+var a ;
+
 function stepZero() {
-  var n = 16;
-  a = new Array(n);
-  var i;
-  for (i=0; i<n; i++) {
-    a[i] = i;
+  a = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+  console.log(a);
+  for (var i=0; i<16;i++ ) {
+    document.getElementById('n'+i).style.background = "grey";//needed when reset
+    document.getElementById('n'+i).style.color = "black";//needed when reset
+    setElementValue('array'+i, a[i]) ;
   }
-  for (i=1; i<17; i++) {
-    setElementValue('array'+i, a[i-1]) ;
-  }
-  for (i=1; i<17; i++) {
-    document.getElementById('a'+i).style.background = "grey";
-    document.getElementById('a'+i).style.color = "black" ;
-    setElementValue('array'+i, a[i-1]) ;
-  }
-  var step = 1;
-  document.getElementById("codeLine1").style.background = codeColor;
-  document.getElementById("codeLine2").style.background = codeColor;
+  step = 1 ;
+  index = -1;
+  swapIndex = 0;
+  document.getElementById("codeloc0").style.background = cb_color ;
+  document.getElementById("codeloc1").style.background = cb_color ;
+  setCodeColors(0) ;
 }
+
 function stepAlgo() {
-  switch(step) {
+  switch (step) {
   case 1:
-    document.getElementById("codeLine1").style.background = highlight;
-    document.getElementById("codeLine2").style.background = codeColor;
+    console.log("step: " + step);
+    console.log("index: " + index + "swapIndex: " + swapIndex);
     index++;
+    console.log("index: " + index + "swapIndex: " + swapIndex);
     setCellColor(index,'blue');
+    setCodeColors(0) ;
     step = 2;
-    break;
+    break ;
   case 2:
-    document.getElementById("codeLine1").style.background = codeColor;
-    document.getElementById("codeLine2").style.background = highlight;
-    var swapIndex = Math.floor(n*Math.random());
+    console.log("step: " + step);
+    console.log("index: " + index + "swapIndex: " + swapIndex);
+    do {
+        swapIndex = Math.floor(16*Math.random());
+    } while (swapIndex == index);
     setCellColor(swapIndex,'red');
+    setCodeColors(1);
     step = 3;
-    break;
+    break ;
   case 3:
+    console.log("step: " + step);
+    console.log("index: " + index + "swapIndex: " + swapIndex);
     var temp = a[index];
     a[index] = a[swapIndex];
-    a[swapIndex] = a[index];
+    a[swapIndex] = temp;
     setCellColor(index,'red');
     setCellColor(swapIndex,'blue');
     setElementValue('array'+index, a[index]); //'array' right name?
     setElementValue('array'+swapIndex, a[swapIndex]);
-    step = 3;
-    break;
+    step = 4;
+    console.log(a);
+    break ;
   case 4:
+    console.log("step: " + step);
     setCellColor(index,'green');
-    setCellColor(swapIndex,'grey');
+    if (swapIndex < index) {
+      setCellColor(swapIndex,'green');
+    } else {
+      setCellColor(swapIndex,'grey');
+    }
     step = 1;
+    if (index==15) {
+      step = 5;
+      break ;
+    }
+    break ;
+  case 5: //this should be case zero where we dont increase index
     break;
   }
+}
+
+function setElementValue(elementId, value) {
+  if (document.getElementById(elementId).innerText!=undefined) {
+    document.getElementById(elementId).innerText = value ;
+  } else {
+    document.getElementById(elementId).textContent = value ;
+  }
+}
+
+function setCellColor(cell, color) {
+  document.getElementById('n'+cell).style.background = color ;
+}
+
+function setCodeColors(line) {
+    document.getElementById("codeloc0").style.background = cb_color ;
+    document.getElementById("codeloc1").style.background = cb_color ;
+    document.getElementById("codeloc"+line).style.background = cbh_color ;
 }
